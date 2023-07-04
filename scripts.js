2e5c7c91a7aa5e99b9f9ca3470380dfaf57176d6
KA11I <75325289+KA11I@users.noreply.github.com>
2023-07-04 23:19:54 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,39 +1,164 @@
-
 document.addEventListener('DOMContentLoaded', function() {
+    function updateCounter() {
+        // Get the current count from local storage or set it to 0 if not present
+        let count = parseInt(getLocalStorage('callCount')) || 0;
+
+        // Display the count on the webpage
+        document.getElementById('count').textContent = count;
+    }
+
+    // Function to set a value in local storage
+    function setLocalStorage(key, value) {
+        localStorage.setItem(key, value);
+    }
+
+    // Function to get a value from local storage
+    function getLocalStorage(key) {
+        return localStorage.getItem(key);
+    }
+
+    // Call the function on page load
+    updateCounter();
+
+    // Rest of your code...
+
+    // Function to increment the counter and update it in local storage
+    function incrementCounter() {
+        // Get the current count from local storage or set it to 0 if not present
+        let count = parseInt(getLocalStorage('callCount')) || 0;
+
+        // Increment the count
+        count++;
+
+        // Set the updated count in local storage
+        setLocalStorage('callCount', count);
+
+        // Display the count on the webpage
+        document.getElementById('count').textContent = count;
+    }
+    function shuffleArray(array) {
+        return array.sort(() => Math.random() - 0.5);
+    }
+
+    function generateLetterCircle(mainWord) {
+        const circleContainer = document.getElementById("letterCircle");
+        circleContainer.innerHTML = "";
+
+        const mainLetters = shuffleArray(mainWord.split(""));
+
+        for (let i = 0; i < mainLetters.length; i++) {
+            const letter = mainLetters[i];
+
+            const angle = (360 / mainLetters.length) * i;
+
+            const letterElement = document.createElement("span");
+            letterElement.className = "letter";
+            letterElement.style.transform = `rotate(${angle}deg) translate(100px) rotate(-${angle}deg)`;
+            letterElement.textContent = letter;
+            circleContainer.appendChild(letterElement);
+        }
+    }
+
+    function getRandomWord(words) {
+        return words[Math.floor(Math.random() * words.length)];
+    }
+
+    function filterWords(words, targetWord) {
+        const filteredWords = [];
+        const targetLetters = targetWord.split('');
+
+        for (let i = 0; i < words.length; i++) {
+            const word = words[i].trim();
+            if (word.length <= targetWord.length && isWordValid(word, targetLetters)) {
+                filteredWords.push(word);
+            }
+        }
+
+        return filteredWords;
+    }
+
+    function isWordValid(word, targetLetters) {
+        const letters = word.split('');
+        const letterCount = {};
+
+        for (let i = 0; i < letters.length; i++) {
+            const letter = letters[i];
+
+            // Check if the letter is already present in the word
+            if (letterCount[letter]) {
+                return false;
+            }
+
+            // Check if the letter is not included in the target letters
+            if (!targetLetters.includes(letter)) {
+                return false;
+            }
+
+            letterCount[letter] = true;
+        }
+
+        return true;
+    }
+
+    function generateWordList(text) {
+        const words = text.split('\n').filter(Boolean);
+        const wordList = [];
+        let targetWord, filteredWords;
+
+        do {
+            targetWord = getRandomWord(words).trim(); // Trim the target word to remove the carriage return character
+            filteredWords = filterWords(words, targetWord);
+        } while (filteredWords.length < 3);
+
+        wordList.push(targetWord);
+        for (let i = 0; i < filteredWords.length; i++) {
+            const word = filteredWords[i].trim();
+            if (!wordList.includes(word)) {
+                wordList.push(word);
+            }
+        }
+
+        return wordList;
+    }
 
 
-      
+    const inputField = document.getElementById("textField");
 
+    let wordList;
+    let wordCount;
+function refresh(){
+    const filename = 'allwords.txt'; // Name der Textdatei
+    fetch(filename)
+        .then(response => response.text())
+        .then(text => {
+            wordList = generateWordList(text);
+            wordCount = wordList.length;
+            console.log(wordList);
+            generateLetterCircle(wordList[0]);
+            inputField.value = "";
 
-
-
+        })
+        .catch(error => {
+            console.error('Fehler beim Laden der Datei:', error);
+        });
+    }
+    refresh();
     var circle = document.getElementById('touch_circle');
     var isDragging = false;
 
-
     function createTrail(x, y) {
-        moveCircle(x - 10 , y - 10);
-    
+        moveCircle(x - 10, y - 10);
 
-
-
-        
         var elem = document.createElement("div");
         elem.setAttribute("class", "trail");
         elem.setAttribute("style", `left: ${x - 10}px; top: ${y - 10}px;`);
         elem.onanimationend = () => {
             elem.remove();
-        }
+        };
         document.body.insertAdjacentElement("beforeend", elem);
-    
     }
-      
 
 
-
-
-    const inputField = document.getElementById("textField");
-    inputField.value = "";
 
     document.addEventListener('mousedown', function(event) {
         if (event.button === 0) {
@@ -44,7 +169,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.addEventListener('mousemove', function(event) {
         if (isDragging) {
-            createTrail(event.clientX, event.clientY)
+            createTrail(event.clientX, event.clientY);
             checkLetterCollision();
         }
     });
@@ -65,8 +190,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('touchmove', function(event) {
         if (isDragging) {
             var touch = event.touches[0];
-            createTrail(touch.clientX, touch.clientY)
-
+            createTrail(touch.clientX, touch.clientY);
             checkLetterCollision();
         }
     });
@@ -89,7 +213,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function hideCircle() {
         circle.style.display = 'none';
+        
+        var letters = document.getElementsByClassName('letter');
+        for (var i = 0; i < letters.length; i++) {
+            var letter = letters[i];
+            letter.style.color = '';
+            letter.classList.remove('used');
+        }
+    
+        if (wordList.includes(inputField.value)) {
+            console.log("yes");
+            var index = wordList.indexOf(inputField.value);
+            if (index > -1) {
+                wordList.splice(index, 1);
+            }
+            console.log(wordList.length +"/" + wordCount)
+            if (wordList.length == 0){
+                incrementCounter();
+                refresh();
+            }
+        } else {
+            console.log("no");
+        }
+        inputField.value = "";
     }
+    
 
     function checkLetterCollision() {
         var letters = document.getElementsByClassName('letter');
@@ -116,28 +264,3 @@ document.addEventListener('DOMContentLoaded', function() {
         );
     }
 });
-
-function generateRandomLetter() {
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const randomIndex = Math.floor(Math.random() * letters.length);
-    return letters.charAt(randomIndex);
-}
-
-function generateLetterCircle() {
-    const circleContainer = document.getElementById("letterCircle");
-    circleContainer.innerHTML = "";
-
-    for (let i = 0; i < 5; i++) {
-        const letter = generateRandomLetter();
-        const angle = (360 / 5) * i;
-
-        const letterElement = document.createElement("span");
-        letterElement.className = "letter";
-        letterElement.style.transform = `rotate(${angle}deg) translate(100px) rotate(-${angle}deg)`;
-        letterElement.textContent = letter;
-
-        circleContainer.appendChild(letterElement);
-    }
-}
-
-generateLetterCircle();
